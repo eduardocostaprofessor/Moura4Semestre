6ee7a7c0678c508b1a447964c8f15bcbc54bb147
Eduardo Costa <eduardo.costa@docente.senai.br>
2026-08-12 09:18:34 -0300

    Desenvolvendo Card

--- React Native/ReactListNative/src/components/taskitem/TaskItem.jsx
+++ React Native/ReactListNative/src/components/taskitem/TaskItem.jsx
@@ -1,10 +1,31 @@
-import { Text, View } from "react-native"
-import { TaskItemStyle } from "./TaskItemStyle"
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { TaskItemStyle } from "./TaskItemStyle";
+
 export const TaskItem = () => {
-    return (
-        <View style={TaskItemStyle.cardBox}>
-            <Text>Tast Item Component</Text>
-        </View>
-    )
-}
+  return (
+    <View style={TaskItemStyle.cardBox}>
+        <Text style={TaskItemStyle.cardText}>Estudar React Native</Text>
+      
+      <TouchableOpacity 
+        style={
+            [
+                TaskItemStyle.cardButton, 
+                TaskItemStyle.CardButtonEditColor
+            ]
+        }
+    >
+        <Image 
+            source={require("../../../assets/edit.png")}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity 
+        style={[TaskItemStyle.cardButton, TaskItemStyle.CardButtonTrashColor]}
+    >
+        <Image 
+            source={require("../../../assets/trash.png")}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 // #31364D
--- React Native/ReactListNative/src/components/taskitem/TaskItemStyle.js
+++ React Native/ReactListNative/src/components/taskitem/TaskItemStyle.js
@@ -1,14 +1,33 @@
 import { StyleSheet } from "react-native";
 
-
 export const TaskItemStyle = StyleSheet.create({
-    cardBox: {
-        // gap: 15,
-        marginBottom: 15,
-        // borderWidth: 2,
-        // borderStyle: "solid",
-        // borderColor: "white",
-        backgroundColor: "#31364D"
+  cardBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    height: 70,
+    padding: 15,
+    marginBottom: 15,
+    borderRadius: 5,
+    backgroundColor: "#31364D",
+  },
+  cardText: {
+    color: "#fff",
+  },
+  cardButton: {
+    width: 45,
+    height: 45,
+    justifyContent: "center",
+    alignItems: "center",
+    
+    borderWidth: 1,
+    borderStyle: "solid"
+  },
 
-    }
-})
+  CardButtonEditColor: {
+    borderColor: "#9ABAEE",
+  },
+  CardButtonTrashColor: {
+    borderColor: "#B75D63",
+  }
+});
